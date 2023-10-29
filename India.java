416e44ff26d1f5b546b818de7239ae0a7a3948b8
SARAVANAN <148688977+Sara4Quantum@users.noreply.github.com>
2023-10-29 23:06:38 +0530

    Add files via upload

---
+++ India.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public abstract class India {
+	static String capital ="New Delhi"; 
+	public India(String primeMinister) {
+		System.out.println("our Prime Minister is " + primeMinister);
+		
+	}
+	
+	abstract void speakLanguage();
+	abstract void eat();
+	abstract void dress();
+
+
+}
