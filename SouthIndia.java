2c542ae3adede18f1adcd00bfd7a783a40578d5b
SARAVANAN <148688977+Sara4Quantum@users.noreply.github.com>
2023-10-29 23:07:23 +0530

    SouthIndia.java

---
+++ SouthIndia.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public abstract class SouthIndia extends India
+{  
+	public SouthIndia(String primeMinister) {
+		super(primeMinister);
+		// TODO Auto-generated constructor stub
+	}
+	void cultivate() {
+	System.out.println("Rice and Wheat cultivation");
+}
+void livingStyle() {
+	System.out.println("Average development");
+}
+}
